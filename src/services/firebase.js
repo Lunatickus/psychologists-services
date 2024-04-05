54295117e3ff9223963e3b4f7b43641bbee184d4
Igor Lunyakin <lunyaigor484@gmail.com>
2024-04-05 13:06:03 +0300

    remove .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTO_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBM58xCHiU9ld_fIMWuIfFZ5XtW-vizgTs",
+  authDomain: "psychologists-services-34aff.firebaseapp.com",
+  databaseURL: "https://psychologists-services-34aff-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "psychologists-services-34aff",
+  storageBucket: "psychologists-services-34aff.appspot.com",
+  messagingSenderId: "929653984407",
+  appId: "1:929653984407:web:9705dd7059f1afc8652dd2",
+  measurementId: "G-S514SV57BM"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
